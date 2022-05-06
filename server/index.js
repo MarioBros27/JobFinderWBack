a96f72f4c13d29fab10721ac254392b5e245405e
MarioBros27 <a01187827@itesm.mx>
2022-05-06 16:59:11 -0600

    deployed to heroku and seemed to work fine

--- server/index.js
+++ server/index.js
@@ -7,22 +7,17 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-// Have Node serve the files for our built React app
-app.use(function(req, res, next) {
-  if ((req.get('X-Forwarded-Proto') !== 'https')) {
-    res.redirect('https://' + req.get('Host') + req.url);
-  } else
-    next();
-});
+// // Have Node serve the files for our built React app
+// app.use(function(req, res, next) {
+//   if ((req.get('X-Forwarded-Proto') !== 'https')) {
+//     res.redirect('https://' + req.get('Host') + req.url);
+//   } else
+//     next();
+// });
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
-// Handle GET requests to /api route
-app.get("/api/images/:project/:name", (req, res) => {
-  const project = req.params.project
-  const name = req.params.name
-  res.sendFile(path.resolve(__dirname,"../client/public/images",project,name))
-});
+
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
