6ec00e6b1ae4ed416d25dfe199eaebc6260ed69f
MarioBros27 <a01187827@itesm.mx>
2022-05-06 18:23:08 -0600

    fixed that photo actually

--- client/src/routes/us.js
+++ client/src/routes/us.js
@@ -3,27 +3,25 @@ export default function Us() {
     return (
         <Grid container direction="column" spacing={0}>
             <Grid item container>
-                
-                    <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/1.png`} />
-                
-                
-                    <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/2.png`} />
-                
-                
-                    <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/3.png`} />
-                
-                
-                    <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/4.png`} />
-                
-                
-                    <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/5.png`} />
-                
-                
-                    <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/6.png`} />
-                
-                
-                    <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/7.png`} />
-                
+
+                <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/1.png`} />
+
+
+                <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/2.png`} />
+
+
+
+                <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/4.png`} />
+
+
+                <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/5.png`} />
+
+
+                <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/6.png`} />
+
+
+                <img style={{ width: "100%", minWidth: "100%", maxWidth: "100%" }} src={`${process.env.PUBLIC_URL}/us/7.png`} />
+
             </Grid>
         </Grid>
     )
